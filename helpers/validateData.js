1fc7b24a36d8fce1ac48b7d06bf9cbbcd9270f66
mjuncobranco <mjuncobranco@gmail.com>
2024-11-13 12:02:47 +0100

    updated validateData function to control users data input

--- helpers/validateData.js
+++ helpers/validateData.js
@@ -1,23 +1,68 @@
+// const validator = require("validator");
+
+// const validateData = (params) => {
+//   let validate_name = validator.isLength(params.name, { min: 3, max: 50 }) && validator.isAlpha(params.name);
+//   let validate_surname = validator.isLength(params.surname, {
+//     min: 4,
+//     max: 50,
+//   }) && validator.isAlpha(params.surname);
+//   let validate_nick = validator.isLength(params.nick, { min: 3, max: 10 });
+//   let validate_password = validator.isLength(params.password, {
+//     min: 8,
+//     max: 15,
+//   }) && validator.isAscii(params.password);
+
+//   let validate_email = validator.isEmail(params.email);
+
+//   if (!validate_name || !validate_surname || !validate_nick || !validate_email || !validate_password) {
+//     throw new Error("Unable to validate.");
+//   }
+  
+// };
+
+// module.exports = {
+//   validateData,
+// };
+
 const validator = require("validator");
 
 const validateData = (params) => {
-  let validate_name = validator.isLength(params.name, { min: 3, max: 50 }) && validator.isAlpha(params.name);
-  let validate_surname = validator.isLength(params.surname, {
-    min: 4,
-    max: 50,
-  }) && validator.isAlpha(params.surname);
-  let validate_nick = validator.isLength(params.nick, { min: 3, max: 10 });
-  let validate_password = validator.isLength(params.password, {
-    min: 8,
-    max: 15,
-  }) && validator.isAscii(params.password);
-
-  let validate_email = validator.isEmail(params.email);
-
-  if (!validate_name || !validate_surname || !validate_nick || !validate_email || !validate_password) {
-    throw new Error("Unable to validate.");
+  // Validar solo los campos que están presentes en `params`
+  
+  // Validación del nombre (si existe)
+  if (params.name) {
+    if (!validator.isLength(params.name, { min: 3, max: 50 }) || !validator.isAlpha(params.name)) {
+      throw new Error("Invalid name: it should be between 3-50 characters and contain only letters.");
+    }
   }
   
+  // Validación del apellido (si existe)
+  if (params.surname) {
+    if (!validator.isLength(params.surname, { min: 4, max: 50 }) || !validator.isAlpha(params.surname)) {
+      throw new Error("Invalid surname: it should be between 4-50 characters and contain only letters.");
+    }
+  }
+
+  // Validación del nickname (si existe)
+  if (params.nick) {
+    if (!validator.isLength(params.nick, { min: 3, max: 10 })) {
+      throw new Error("Invalid nick: it should be between 3-10 characters.");
+    }
+  }
+
+  // Validación de la contraseña (si existe)
+  if (params.password) {
+    if (!validator.isLength(params.password, { min: 8, max: 15 }) || !validator.isAscii(params.password)) {
+      throw new Error("Invalid password: it should be between 8-15 characters and contain only ASCII characters.");
+    }
+  }
+
+  // Validación del email (si existe)
+  if (params.email) {
+    if (!validator.isEmail(params.email)) {
+      throw new Error("Invalid email format.");
+    }
+  }
 };
 
 module.exports = {
