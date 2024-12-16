e38e47716f814ba811ab7cf275e879e23d289fba
mjuncobranco <mjuncobranco@gmail.com>
2024-12-16 20:49:35 +0100

    updated and strengthen validateData function to control errors on data

--- helpers/validateData.js
+++ helpers/validateData.js
@@ -1,92 +1,55 @@
-// const validator = require("validator");
-
-// const validateData = (params) => {
-//   let validate_name = validator.isLength(params.name, { min: 3, max: 50 }) && validator.isAlpha(params.name);
-//   let validate_surname = validator.isLength(params.surname, {
-//     min: 4,
-//     max: 50,
-//   }) && validator.isAlpha(params.surname);
-//   let validate_nick = validator.isLength(params.nick, { min: 3, max: 10 });
-//   let validate_password = validator.isLength(params.password, {
-//     min: 8,
-//     max: 15,
-//   }) && validator.isAscii(params.password);
-
-//   let validate_email = validator.isEmail(params.email);
-
-//   if (!validate_name || !validate_surname || !validate_nick || !validate_email || !validate_password) {
-//     throw new Error("Unable to validate.");
-//   }
-  
-// };
-
-// module.exports = {
-//   validateData,
-// };
-
 const validator = require("validator");
-
-const validateData = (params) => {
-  // Validar solo los campos que están presentes en `params`
-  
-  // Validación del nombre (si existe)
-  if (params.name) {
-    if (!validator.isLength(params.name, { min: 2, max: 50 }) || !validator.isAlpha(params.name)) {
-      throw new Error("Invalid name: it should be between 3-50 characters and contain only letters.");
+const validateData = (params, field = null) => {
+  // Validar un campo específico si se pasa como argumento
+  if (field) {
+    if (field === "name" && params.name) {
+      if (!validator.isLength(params.name, { min: 2, max: 50 }) || !validator.isAlpha(params.name)) {
+        throw new Error("Invalid name: it should be between 2-50 characters and contain only letters.");
+      }
     }
-  }
- 
-   // Validación del apellido (si existe)
-  if (params.surname) {
-    if (!validator.isLength(params.surname, { min: 2, max: 50 }) || !validator.isAlpha(params.surname)) {
-      throw new Error("Invalid surname: it should be between 4-50 characters and contain only letters.");
+
+    if (field === "surname" && params.surname) {
+      if (!validator.isLength(params.surname, { min: 2, max: 50 }) || !validator.isAlpha(params.surname)) {
+        throw new Error("Invalid surname: it should be between 2-50 characters and contain only letters.");
+      }
     }
-  }
 
-  // Validación del nickname (si existe)
-  if (params.nick) {
-    if (!validator.isLength(params.nick, { min: 3, max: 10 }) ||
-    !validator.matches(params.nick, /^[a-zA-Z0-9]+$/)) {
-      throw new Error("Invalid nick: it should be between 3-10 characters and contain only letters and numbers.");
+    if (field === "nick" && params.nick) {
+      if (!validator.isLength(params.nick, { min: 3, max: 10 }) || !validator.matches(params.nick, /^[a-zA-Z0-9]+$/)) {
+        throw new Error("Invalid nick: it should be between 3-10 characters and contain only letters and numbers.");
+      }
     }
-  }
 
-  // Validación de la contraseña (si existe)
-  if (params.password) {
-    if (!validator.isLength(params.password, { min: 8, max: 15 }) || !validator.isAscii(params.password)) {
-      throw new Error("Invalid password: it should be between 8-15 characters and contain only ASCII characters.");
+    if (field === "email" && params.email) {
+      if (!validator.isEmail(params.email)) {
+        throw new Error("Invalid email format.");
+      }
     }
-  }
-  // Verificar que contiene al menos una letra mayúscula
-  if (!/[A-Z]/.test(params.password)) {
-    throw new Error("Invalid password: it must include at least one uppercase letter.");
-  }
 
-  // Verificar que contiene al menos un número
-  if (!/\d/.test(params.password)) {
-    throw new Error("Invalid password: it must include at least one number.");
-  }
-
-  // Verificar que contiene al menos un símbolo especial
-  if (!/[!@#$%^&*]/.test(params.password)) {
-    throw new Error("Invalid password: it must include at least one special character (!@#$%^&*).");
-  }
-
-  // Verificar que todos los caracteres son ASCII
-  if (!validator.isAscii(params.password)) {
-    throw new Error("Invalid password: it must only contain ASCII characters.");
-  }
-
-
-  // Validación del email (si existe)
-  if (params.email) {
-    if (!validator.isEmail(params.email)) {
-      throw new Error("Invalid email format.");
+    if (field === "password" && params.password) {
+      if (
+        !validator.isLength(params.password, { min: 8, max: 15 }) ||
+        !/[A-Z]/.test(params.password) ||
+        !/\d/.test(params.password) ||
+        !/[!@#$%^&*]/.test(params.password) ||
+        !validator.isAscii(params.password)
+      ) {
+        throw new Error(
+          "Invalid password: it must be 8-15 characters long, include an uppercase letter, a number, and a special character (!@#$%^&*)."
+        );
+      }
     }
-  };
+    return; // No errores para el campo específico
+  }
+
+  // Validar todos los campos (por si no se pasa un campo específico)
+  if (params.name) validateData(params, "name");
+  if (params.surname) validateData(params, "surname");
+  if (params.nick) validateData(params, "nick");
+  if (params.email) validateData(params, "email");
+  if (params.password) validateData(params, "password");
 };
-
 
 module.exports = {
   validateData,
-};
+  };
